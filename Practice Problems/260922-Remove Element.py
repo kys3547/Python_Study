00500edc473aae5cc63d0f6cfd0a38d7056359c3
APChoGath <kkyyss3547@gmail.com>
2026-09-22 18:13:58 +0900

    Editied format

--- Practice Problems/260922-Remove Element.py
+++ Practice Problems/260922-Remove Element.py
@@ -9,7 +9,6 @@ Consider the number of elements in nums which are not equal to val be k, to get 
 Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
 Return k.
 
-
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
         k = len(nums)
@@ -24,7 +23,8 @@ class Solution:
             else:
                 i += 1
 
-        return k"""
+        return k
+"""
 
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
@@ -32,5 +32,20 @@ class Solution:
 
         for i in range(len(nums)):
             if nums[i] != val:
+                nums[k] = nums[i]
                 k += 1
-            
+
+        return k
+
+"""
+Note
+
+Using two pointers is hard... I always have no clue until I see hints.
+My first soolution uses two pointers but one pointer starts at the end of the list.
+So the solution got quite complicated, and it looks dirty.
+
+The second solution uses two pointers, but this time both of them starts from
+the beginning of the list. When it encounters val, the writing pointer stays.
+The reading pointer continues and search for non-vals. Once found, it replaces
+val(k) with non-val(i)
+"""
